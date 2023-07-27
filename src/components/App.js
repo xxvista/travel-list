611e01cd70b9f324b4e6b078f332b63706894d27
xxvista <abdelkafiabdellah419@gmail.com>
2023-07-27 09:22:43 +0100

    finale version

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charge", quantity: 1, packed: true },
+];
+
+export default function App() {
+  const [items, setItems] = useState(initialItems);
+
+  function handleDeleteAllItems() {
+    if (!items.length) return;
+    const confirm = window.confirm("Are you sure you  wanna delete all items");
+    if (confirm) setItems([]);
+  }
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+        onDeleteAllItems={handleDeleteAllItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
